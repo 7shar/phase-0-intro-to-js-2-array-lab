332cb95c3211c6aabc8738b5476a5b4df775d17f
Sharon Nyawira <sharon.mwandiki@student.moringaschool.com>
2024-03-24 19:29:42 -0400

    completed the js array lab phase 0 for phase 1

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution here!
+// initial array of cat names
+const cats = ["Milo","Otis", "Garfield",]
+console.log(cats)
+const destructivelyAppendCat = (name) => cats.push("Ralph") 
+const destructivelyPrependCat = (name) => cats.unshift("Bob")   
+const destructivelyRemoveLastCat = (name) => cats.pop()
+const destructivelyRemoveFirstCat = (name) => cats.shift()   
+const appendCat = (name) => [...cats,"Broom" ]
+const prependCat = (name) => ["Arnold", ...cats ]
+const removeLastCat = (name) => cats.slice(0, 2)
+const removeFirstCat = (name) => cats.slice(1)
